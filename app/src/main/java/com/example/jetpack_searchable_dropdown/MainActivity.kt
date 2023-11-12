4d40115fd600c58cd60745e5e062ab8cbd348a90
mejdi14 <mejdiclubiste@gmail.com>
2023-11-12 23:50:26 +0100

    first attempt for the button animation

--- app/src/main/java/com/example/jetpack_searchable_dropdown/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_searchable_dropdown/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
@@ -27,6 +30,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import com.example.jetpack_searchable_dropdown.ui.theme.JetpackSearchableDropdownTheme
 
@@ -71,13 +75,28 @@ class MainActivity : ComponentActivity() {
                     .align(Alignment.CenterStart)
                     .padding(vertical = 16.dp)
             )
-            Icon(
-                imageVector = Icons.Filled.ArrowDropDown,
-                contentDescription = "Toggle Dropdown",
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .rotate(rotationAngle.value)
-            )
+            AnimatedVisibility(
+                visible = expanded,
+                enter = expandVertically(
+                    animationSpec = tween(durationMillis = 3000)
+                ) + fadeIn(
+                    initialAlpha = 0.3f,
+                    animationSpec = tween(durationMillis = 3000)
+                ),
+                exit = shrinkVertically(
+                    animationSpec = tween(durationMillis = 3000)
+                ) + fadeOut(
+                    animationSpec = tween(durationMillis = 3000)
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowDropDown,
+                    contentDescription = "Toggle Dropdown",
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .rotate(rotationAngle.value)
+                )
+            }
         }
         Spacer(modifier = Modifier.height(10.dp))
 
@@ -85,15 +104,15 @@ class MainActivity : ComponentActivity() {
         AnimatedVisibility(
             visible = expanded,
             enter = expandVertically(
-                animationSpec = tween(durationMillis = 300)
+                animationSpec = tween(durationMillis = 3000)
             ) + fadeIn(
                 initialAlpha = 0.3f,
-                animationSpec = tween(durationMillis = 300)
+                animationSpec = tween(durationMillis = 3000)
             ),
             exit = shrinkVertically(
-                animationSpec = tween(durationMillis = 300)
+                animationSpec = tween(durationMillis = 3000)
             ) + fadeOut(
-                animationSpec = tween(durationMillis = 300)
+                animationSpec = tween(durationMillis = 3000)
             )
         ) {
             Column(
@@ -129,5 +148,57 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+}
+
+@Composable
+fun CustomAnimatedVisibility(
+    visible: Boolean,
+    content: @Composable () -> Unit
+) {
+    // Create an Animatable for scale and alpha, default to the end values of the shrink and fade in
+    val scale = remember { Animatable(0.8f) }
+    val alpha = remember { Animatable(0f) }
+
+    // Handle the enter animation sequence
+    LaunchedEffect(visible) {
+        if (visible) {
+            // Animate from shrink and faded to expanded and visible
+            scale.animateTo(1f, animationSpec = tween(300))
+            alpha.animateTo(1f, animationSpec = tween(300))
+        }
+    }
+
+    // Use the updateTransition API to handle enter and exit together
+    val transition = updateTransition(targetState = visible, label = "visibilityTransition")
+    val animatedScale by transition.animateFloat(
+        transitionSpec = { tween(300) },
+        label = "scaleTransition"
+    ) { state ->
+        if (state) 1f else 0.8f
+    }
+    val animatedAlpha by transition.animateFloat(
+        transitionSpec = { tween(300) },
+        label = "alphaTransition"
+    ) { state ->
+        if (state) 1f else 0f
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(200.dp).background(Color.Gray)
+    ) {
+        if (visible) {
+            Box(
+                modifier = Modifier
+                    .graphicsLayer {
+                        scaleX = animatedScale * scale.value
+                        scaleY = animatedScale * scale.value
+                        this.alpha = animatedAlpha * alpha.value
+                    }
+                    .background(MaterialTheme.colorScheme.primary)
+                    .size(100.dp)
+            )
+        }
+    }
 }
 
